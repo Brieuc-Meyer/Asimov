bd3abc98bb335993dafd5a976a77ef715d5dbfb3
EscolanoA <escolanoallan4@gmail.com>
2023-04-25 14:47:52 +0200

    Pour bri

--- Asimov Student Manager/AsimovStudentManager/AsimovStudentManager/Form1.Designer.cs
+++ Asimov Student Manager/AsimovStudentManager/AsimovStudentManager/Form1.Designer.cs
@@ -226,7 +226,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1171, 724);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Vue notes éleve";
+            this.tabPage2.Text = "Main éleve";
             // 
             // btn_DisconnectEleve
             // 
